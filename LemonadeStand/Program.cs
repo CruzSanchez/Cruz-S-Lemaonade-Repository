481128712f89acea7a55a45c1fe932600a1185d3
unknown <cruzsanchez9209@gmail.com>
2018-07-18 10:04:45 -0500

    Added ability to input name

--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -6,9 +6,12 @@ namespace LemonadeStand
     {
         static void Main(string[] args)
         {
-            
 
-
+			Console.WriteLine("Hello! Seems you would like to start a lemonade stand. What would you like to name it?");
+			var lemonadeStandName = Console.ReadLine();
+			Console.ReadLine();
+			Console.WriteLine("Ahhhh yes! " + lemonadeStandName + " thats a great name!");
+			Console.ReadLine();
 
 
 
