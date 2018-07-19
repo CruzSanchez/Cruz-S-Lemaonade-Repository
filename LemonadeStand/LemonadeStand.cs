d00e2dda90bb304c147137f6b0410b1a0b539cc6
unknown <cruzsanchez9209@gmail.com>
2018-07-19 14:06:32 -0500

    Added Class lemonade stand and completely remodeled program

---
+++ LemonadeStand/LemonadeStand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LemonadeStand
+{
+    class LemonadeStand
+    {
+		public string standName { get; set; }
+		public decimal customerPrice { get; set; }
+		public decimal sellerCost { get; set; }
+		public int numOfCups { get; set; }
+		
+
+
+		
+		public decimal getTotalRevenue()
+		{
+			return numOfCups * customerPrice;
+		}
+		
+		public decimal getTotalExpense()
+		{
+			return numOfCups * sellerCost;
+		}
+
+		public decimal getTotalProfit()
+		{
+			return getTotalRevenue() - getTotalExpense();
+		}
+
+
+	}
+
+	
+}
--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -1,115 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LemonadeStand
 {
-    class Program
-    {
+	class Program
+	{
 		static void Main(string[] args)
 		{
-			LemonadeStand newUserStand = new LemonadeStand();
-			LemonadeStand nextStand = new LemonadeStand();
+			Console.WriteLine("Lets open some lemonade stands, how many would you like to open?");
+			int numofstands = int.Parse(Console.ReadLine());
 
-			Console.WriteLine("Hello! Seems you would like to start a lemonade stand. What would you like to name it?");
-			newUserStand.standName = Console.ReadLine();
-			
-			Console.WriteLine("Ahhhh yes! " + newUserStand.standName + " thats a great name!");
-			Console.WriteLine();
-			Console.WriteLine("Lets go ahead and open a second one, whats it's name?");
-			Console.WriteLine();
+			List<LemonadeStand> stands = new List<LemonadeStand>();
 
-			nextStand.standName = Console.ReadLine();
-			Console.WriteLine(nextStand.standName + " Excellent! I think I like the first one better honestly." );
-			Console.WriteLine();
-			Console.WriteLine("Well we will need some basic information, like how many cups of lemonade you plan to sell in your first stand?");
+			for (int i = 0; i < numofstands; i++)
+			{
+				LemonadeStand newstand = new LemonadeStand();
 
-			newUserStand.numOfCups = Int32.Parse(Console.ReadLine()); // Variable for cups to sell converted to an int.
-			if (newUserStand.numOfCups < 25)
-			{
-				Console.WriteLine("Well thats a pretty low number. Your loss I suppose.");
-			}
-			else
-			{
-				Console.WriteLine("Hmmm, very good!");
-			}
+				Console.WriteLine("What is the name of location " + (i + 1) + "?");
+				newstand.standName = Console.ReadLine();
 
-			Console.WriteLine();
-			Console.WriteLine("How many will the second stand sell?");
-			nextStand.numOfCups = Int32.Parse(Console.ReadLine()); // Variable for cups to sell converted to an int.
-			if (nextStand.numOfCups < 25)
-			{
-				Console.WriteLine("Your not exactly grasping this whole entrepreneur thing are you?");
-			}
-			else
-			{
-				Console.WriteLine("Awesome!");
-			}
+				Console.WriteLine("How many cups will this location sell?");
+				newstand.numOfCups = int.Parse(Console.ReadLine());
 
-			Console.WriteLine();
-			Console.WriteLine("So how much will you sell the lemonade for?");
+				Console.WriteLine("What will your price to sell the lemonade be?");
+				newstand.customerPrice = decimal.Parse(Console.ReadLine());
 
-			
-			newUserStand.customerPrice = decimal.Parse(Console.ReadLine()); // Variable for price to sell to customers converted to an decimal.
-			nextStand.customerPrice = newUserStand.customerPrice;
-			if(newUserStand.customerPrice > 2.5m)
-			{
-				Console.WriteLine("You might want to reconsider, thats highway robbery, but hey it's your bussiness.");
-			}
-			else
-			{
-				Console.WriteLine("Thats a pretty basic price. If thats the case... ");
-			}
+				Console.WriteLine("How much does it cost you to make the lemonade");
+				newstand.sellerCost = decimal.Parse(Console.ReadLine());
 
-			Console.WriteLine();
-			Console.WriteLine("How much does it cost you to make the lemonade??");
+				Console.WriteLine("This stands total revenue will be: " + newstand.getTotalRevenue());
+				Console.WriteLine("The stands total expenses are: " + newstand.getTotalExpense());
+				Console.WriteLine("Your total profit of " + newstand.standName + " is " + newstand.getTotalProfit());
 
-			newUserStand.sellerCost = decimal.Parse(Console.ReadLine()); // Variable for the users cost converted to decimal.
-			nextStand.sellerCost = newUserStand.sellerCost;
-			if(newUserStand.sellerCost >= newUserStand.customerPrice)
-			{
-				Console.WriteLine("I can already tell this won't end well....");
-			}
-
-			Console.WriteLine();
-			decimal totalRevenue = newUserStand.numOfCups * newUserStand.customerPrice - newUserStand.numOfCups * newUserStand.sellerCost;
-			
-			Console.WriteLine("Well if I calculate that all together, this is how much cash you'll be making in the first stand! " + totalRevenue);
-			Console.WriteLine();
-			if (totalRevenue > 0)
-			{
-				Console.WriteLine("Hey, thats great you'er in the green. Keep at it and you'll be in the big leagues like yours truly. ;)");
-
-
-			}
-			else if (totalRevenue < 0)
-			{
-				Console.WriteLine(".... Wait you won't be making any money at all... O__O ... YOU'RE LOSING MONEY!!!!! Close up shop");
-				Console.WriteLine("and try again after your chapter 11.");
-			}
-			else if (totalRevenue == 0)
-			{
-				Console.WriteLine("Yikes... barely broke even on that business model. Maybe go back to the whiteboard and rethink things.");
-				Console.WriteLine("Perhaps increase your price? You could also find a less expensive vendor for supplies.");
-			}
-
-			Console.WriteLine();
-			decimal totalRevenue2 = nextStand.numOfCups * nextStand.customerPrice - nextStand.numOfCups * nextStand.sellerCost;
-			Console.WriteLine("Now for your second stand... Ah yes here is the figure:" + totalRevenue2);
-			Console.WriteLine();
-			if(totalRevenue2 > 0)
-			{
-				Console.WriteLine("Nice! We are making some $$$ now");
-			}
-			else if (totalRevenue2 == 0)
-			{
-				Console.WriteLine(".... well lets try harder next time.");
-			}
-			else if(totalRevenue2 < 0)
-			{
-				Console.WriteLine("You'll be out of money in less than a month, Change something or close up.");
+				stands.Add(newstand);
 			}
 
 			Console.ReadLine();
-        }
 
-    }
+		}
+	}
 }
+
+
+
